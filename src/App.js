b6667618350e6a71510a90496f35af68aea4f567
Pham Duc Long <longblue2001@gmail.com>
2023-07-13 01:24:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("Hanoi");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f98612cf10749accbbb36bcd446601b3`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+      });
+    }
+  };
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setData(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      {data && (
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
+              <h1>{Math.round(data.main.temp - 273)}°C</h1>
+            </div>
+            <div className="description">
+              <p>{data.weather[0].main}</p>
+              <img
+                src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              <p className="bold">{Math.round(data.main.feels_like - 273)}°C</p>
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              <p className="bold">{data.main.humidity}%</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wild">
+              <p className="bold">{data.wind.speed} MPH</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
